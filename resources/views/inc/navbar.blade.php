9ba544a502a161de915437ca9143bf311c46f16f
Paulus Bangun <paulus.bangun@shopee.com>
2020-02-28 00:54:27 +0700

    eps 7

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -19,6 +19,11 @@
           <a class="nav-link" href="/posts">Posts</a>
         </li>
       </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a href="/posts/create">Create Post</a>
+        </li>
+      </ul>
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
         <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
 </div>
     <a href="/posts" class="btn btn-default">Back</a>
     <div class="card p-3 mt-3 mb-3">
-        <h3>{{$post->body}}</h3>
+        <h3>{!!$post->body!!}</h3>
     <small>Written {{$post->created_at}}</small>
     </div>
 @endsection
