0f64907c3a3172223268d695ca1103548168a346
Paulus Bangun <paulus.bangun@shopee.com>
2020-02-28 01:13:03 +0700

    eps 8

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,4 +9,9 @@
         <h3>{!!$post->body!!}</h3>
     <small>Written {{$post->created_at}}</small>
     </div>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">edit</a>
+    {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => "pull-right"]) !!}
+    {{Form::hidden('_method', 'DELETE')}}
+    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!! Form::close()!!}
 @endsection
